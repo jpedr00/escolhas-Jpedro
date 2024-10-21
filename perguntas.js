2c3b653f67b55863aa52f78c867f4c03f791c6ca
jpedr00 <00001093213164sp@al.educacao.sp.gov.br>
2024-10-21 11:55:56 -0300

    animes

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Pokemon',
+    'Qual é o pokemon mais popular do mundo?',
+    'É o Charizard (menção honrosa ao scepitile que é o melhor pokemon que existe)'
+)
+
+criaCartao(
+    'Yu-Gi-Oh',
+    'Qual a carta principal de Kaiba?',
+    'Dragão Branco de Olhos Azuis'
+)
+
+criaCartao(
+    'Naruto',
+    'Qual técnica tem a habilidade de criar um fogo que não se apaga?',
+    'Amaterasu'
+)
+
+criaCartao(
+    'Dragon ball Z',
+    'Como se chama do deus da destruição do universo 6?',
+    'Champa'
+)
